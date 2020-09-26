34e98a02ebeb8d478feae583912db670e82a10d3
ShaikSaqibHussain <52991072+saqib-1998@users.noreply.github.com>
2020-09-26 12:53:28 -0400

    Update hello world .java

--- hello world .java
+++ hello world .java
@@ -1,5 +1,12 @@
-class hello world {
+class helloworld 
+    {
+    
     public static void main(arg[] String) {
+    // here we are going to printt the file
         System.out.println("hello hussain");
+   // Save: Go to cmd and save the file as helloworld.java
+   //  Execution : Just go to the file and go to dir you saved "javac helloworld.java
+   // Run : Just need to run the file go to the file and type as " java helloworld "
     } 
+    
 }
